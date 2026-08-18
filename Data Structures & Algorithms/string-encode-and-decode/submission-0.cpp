d6514eccacd3a306eeac6a882ed5db09fd64c46e
Ynahammed <naveedahammed_yadiki@srmap.edu.in>
2026-08-18 22:53:25 +0530

    Add: string-encode-and-decode - submission-0

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-0.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    string encode(vector<string>& strs) {
+        string encoded = "";
+
+        for (string& s : strs) {
+            encoded += to_string(s.size()) + "#" + s;
+        }
+
+        return encoded;
+    }
+
+    vector<string> decode(string s) {
+        vector<string> result;
+        int i = 0;
+
+        while (i < s.size()) {
+            int j = i;
+
+            // Find '#'
+            while (s[j] != '#') {
+                j++;
+            }
+
+            // Get length
+            int len = stoi(s.substr(i, j - i));
+
+            // Extract string
+            string word = s.substr(j + 1, len);
+            result.push_back(word);
+
+            // Move to next encoded string
+            i = j + 1 + len;
+        }
+
+        return result;
+    }
+};
